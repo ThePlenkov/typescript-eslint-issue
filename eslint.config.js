1eebcad30efd639c70de480087d3de3f00fd3825
Petr Plenkov <petr.plenkov@gmail.com>
2024-04-15 15:12:33 +0000

    fixing config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import nxEslintPlugin from '@nx/eslint-plugin';
+import * as jsonParser from 'jsonc-eslint-parser';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
+  { files: ["*.ts"],
+    extends: [...tseslint.configs.recommendedTypeChecked]    ,
     languageOptions: {
       parserOptions: {                
         EXPERIMENTAL_useProjectService: true,
@@ -15,4 +16,14 @@ export default tseslint.config(
     },
   },
   { plugins: { '@nx': nxEslintPlugin } },
+  {
+    files: ['*.json'],    
+    //...tseslint.configs.disableTypeChecked,    
+    languageOptions: {
+      parser: jsonParser
+    },
+    rules: {
+      '@nx/dependency-checks': 'error',
+    },
+  }
 );
--- libs/a/eslint.config.js
+++ libs/a/eslint.config.js
@@ -1,14 +1,4 @@
 import baseConfig from '../../eslint.config.js'
 import tseslint from 'typescript-eslint';
-import * as jsonParser from 'jsonc-eslint-parser';
 
-export default tseslint.config(
-  ...baseConfig,
-  // {
-  //   languageOptions: {
-  //     parser: jsonParser,
-  //   },
-  //   files: ['**/*.json'],
-  //   rules: { '@nx/dependency-checks': 'error' },
-  // }
-);
+export default tseslint.config(...baseConfig);
